9035b7e7fd0969fa0ac3f83b218cfde51b6de8b1
Lucas Rocha <lucasrocha.png@gmail.com>
2024-12-22 14:25:44 -0300

    Geometry Rendering

--- src/texture.cpp
+++ src/texture.cpp
@@ -5,30 +5,18 @@ SDL_Texture* gTexture = nullptr;
 
 SDL_Texture* load_texture(const char* path)
 {
-    //Load image at specified path
-    SDL_Surface* loadedSurface = IMG_Load(path);
-    if(loadedSurface == nullptr){
-        SDL_Log("Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError());
-        return nullptr;
-    }
-
     //Create texture from surface pixels
-    SDL_Texture* newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+    SDL_Texture* newTexture = IMG_LoadTexture(gRenderer, path);
     if(newTexture == nullptr){
         SDL_Log("Unable to create texture from %s! SDL Error: %s\n", path, SDL_GetError());
     }
-
-    //Get rid of old loaded surface
-    SDL_FreeSurface(loadedSurface);
 
     return newTexture;
 }
 
 Status load_media(void){
     //Load PNG texture
-    gTexture = load_texture("assets/images/texture.png");
-    if(gTexture == nullptr) return ERR;
-
+    
     return OK;
 }
 
